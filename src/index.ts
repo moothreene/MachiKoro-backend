d545918233a18cc743dae7def3b27bc05b485e69
Martin <moothreene@gmail.com>
2024-06-11 11:44:00 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,106 @@
+import express, { Express, Request, Response } from 'express';
+import { createServer, get } from 'node:http';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import { UserModel } from './models/User';
+
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: process.env.CORS_ORIGIN },
+  connectionStateRecovery: {},
+});
+
+function getRandomId() {
+  return Math.random().toString(36).substring(7);
+}
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server ');
+});
+
+io.on('connection', async (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('chat message', async (msg) => {
+    const userDoc = await UserModel.findOne({ socketId: socket.id });
+    if (userDoc) {
+      io.to(userDoc.roomId).emit('chat message', msg);
+    }
+  });
+
+  socket.on('host', async () => {
+    const id = getRandomId();
+    socket.join(id);
+    try {
+      await UserModel.create({ socketId: socket.id, roomId: id });
+    } catch (err) {
+      console.log(err);
+    }
+    socket.emit('hosted', id);
+  });
+
+  socket.on('join', async (id: string) => {
+    const UserDoc = await UserModel.findOne({ roomId: id });
+    console.log(UserDoc);
+    if (!UserDoc) return socket.emit('invalidRoom');
+    socket.join(id);
+    try {
+      await UserModel.create({ socketId: socket.id, roomId: id });
+    } catch (err) {
+      console.log(err);
+    }
+    socket.emit('joined', id);
+    try {
+      const userCount = await UserModel.countDocuments({ roomId: id });
+      if (userCount === 2) {
+        io.to(id).emit('startGame');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  socket.on('roll', async (msg) => {
+    const userDoc = await UserModel.findOne({ socketId: socket.id });
+    if (userDoc) {
+      io.to(userDoc.roomId).emit('roll', msg);
+    }
+  });
+
+  socket.on('confirmRoll', async () => {
+    const userDoc = await UserModel.findOne({ socketId: socket.id });
+    if (userDoc) {
+      io.to(userDoc.roomId).emit('confirmRoll');
+    }
+  });
+
+  socket.on('buy', async (msg) => {
+    const userDoc = await UserModel.findOne({ socketId: socket.id });
+    if (userDoc) {
+      io.to(userDoc.roomId).emit('buy', msg);
+    }
+  });
+
+  socket.on('nextTurn', async () => {
+    const userDoc = await UserModel.findOne({ socketId: socket.id });
+    if (userDoc) {
+      io.to(userDoc.roomId).emit('nextTurn');
+    }
+  });
+
+  socket.on('disconnect', async () => {
+    const roomMap = io.sockets.adapter.rooms;
+    const arr = Array.from(roomMap.keys());
+    await UserModel.deleteMany({ roomId: { $nin: arr } });
+    console.log('a user disconnected');
+  });
+});
+
+mongoose.connect(process.env.DATABASE_URL as string).then(() => {
+  server.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+});
---
+++ src/models/User.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+  socketId: { type: String, required: true, min: 4, unique: true },
+  roomId: { type: String, required: true, min: 2 },
+});
+
+export const UserModel = model('User', UserSchema, 'Users');
+
+// export default UserModel;
